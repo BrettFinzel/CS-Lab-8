2c18f9273511b4a7f1306aa5328fd97d7aa60aef
your user name <your email>
2023-11-07 16:21:46 -0500

    sec commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import BrowserRouter from 'react-router-dom';
+import Routes from 'react-router-dom';
+import Route from 'react-router-dom';
 
 function App() {
   return (
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <div className="App">
       <h1>CS 230L</h1>
 	<h2>Section 002</h2>
@@ -11,5 +24,8 @@ function App() {
     </div>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 export default App;
